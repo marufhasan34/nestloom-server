b3511d8796dc63bfaf89d692831814f7f869650a
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-06-24 11:20:28 +0600

    add-property api created

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express()
 const port = 5000
+const cors = require('cors');
 require('dotenv').config()
+app.use(cors());
+app.use(express.json())
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
@@ -25,6 +29,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    
+    const database = client.db('nestloom_db');
+    const propertyCollection = database.collection('property')
+
+    app.post('/api/property', async (req,res) => {
+      const property = req.body;
+      const result = await propertyCollection.insertOne(property);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

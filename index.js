a4ee7448d3b79dcc7e5294e919aa2c26826e0916
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-06-28 06:22:59 +0600

    subscription based Payment integration api added

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const app = express()
-const port = 5000
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+require("dotenv").config();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const uri = process.env.MONGODB_URI;
 
@@ -22,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,33 +28,60 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("nestloom_db");
+    const propertyCollection = database.collection("property");
+    const subscriptionsCollection = database.collection("subscriptions");
+   const userCollection = database.collection("user"); 
+
+
+    await subscriptionsCollection.createIndex({ sessionId: 1 }, { unique: true });
     
-    const database = client.db('nestloom_db');
-    const propertyCollection = database.collection('property')
 
-    app.get('/api/property', async(req,res) => {
-      const query = {}
-      if(req.query.myPropertyId){
-        query.myPropertyId = req.query.myPropertyId
-      }
-      if(req.query.status){
-        query.status = req.query.status
-      }
-      const cursor = propertyCollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.post("/subscription", async (req, res) => {
+      const { sessionId, userId, priceId } = req.body;
 
-    app.post('/api/property', async (req,res) => {
+      const isExist = await subscriptionsCollection.findOne({sessionId})
+      if(isExist){
+        return res.json({msg: 'Already Exist!'})
+      }
+      await subscriptionsCollection.insertOne({
+        sessionId,
+        userId,
+        priceId,
+      });
+
+      await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        { $set: { plan: "pro" } },
+      );
+
+      res.json({ msg: "Payment successful!" });
+    });
+
+    app.get("/api/property", async (req, res) => {
+      const query = {};
+      if (req.query.myPropertyId) {
+        query.myPropertyId = req.query.myPropertyId;
+      }
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+      const cursor = await propertyCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/api/property", async (req, res) => {
       const property = req.body;
       const result = await propertyCollection.insertOne(property);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -64,9 +89,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
